4edb99efc1a53d8d51a0c1c2bb494a30352ce0e0
M. J. Fromberger <michael.j.fromberger@gmail.com>
2026-05-08 19:28:12 -0700

    Add copyright headers

--- mboxlib.go
+++ mboxlib.go
@@ -1,3 +1,5 @@
+// Copyright (C) Michael J. Fromberger. All Rights Reserved.
+
 // Package mboxlib implements basic iteration and parsing of Unix mailbox
 // (mbox) format files. Presently, only the "classic" format is supported,
 // using "From_" lines to delimit messages.
